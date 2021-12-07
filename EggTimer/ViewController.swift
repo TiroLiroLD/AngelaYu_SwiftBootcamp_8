9f176672ea64f5a1e61c97c69ba5c24964f2156e
Leone <leone.souza@edu.unirio.br>
2021-12-07 13:38:02 -0300

    <L81>implement control flow for egg buttons

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
 
-
+    @IBAction func onEggSelected(_ sender: UIButton) {
+        if sender.currentTitle == "Soft" {
+            print(softTime)
+        } else if sender.currentTitle == "Medium" {
+            print(mediumTime)
+        } else if sender.currentTitle == "Hard" {
+            print(hardTime)
+        }
+        /*
+        switch sender.currentTitle{
+        case "Soft":
+            print(softTime)
+        case "Medium":
+            print(mediumTime)
+        case "Hard":
+            print(hardTime)
+        default: break
+        }
+        */
+    }
+    
 }
